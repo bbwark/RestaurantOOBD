9da76df288cdc0ca1f287a8287958e6d1b58df8c
bbwark <devivobruno01@gmail.com>
2022-02-08 18:16:06 +0100

    Adding some utilities

---
+++ OO/PopolazioneDB/Sala.sql
@@ -0,0 +1,9 @@
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Prima_Sala1"', 1);
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Seconda_Sala1"', 1);
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Terza_Sala1"', 1);
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Prima_Sala2"', 2);
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Seconda_Sala2"', 2);
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Terza_Sala2"', 2);
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Prima_Sala3"', 3);
+INSERT INTO "Sala" ("Nome_Sala", "ID_Ristorante") VALUES ('"Seconda_Sala3"', 3);
+
